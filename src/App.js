b709ab5dad6cc5a9068cc91b3abb063a0c6e8652
Shehan wijeratne <shehanranjukaw5@gmail.com>
2023-12-14 00:12:44 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './layout/navbar';
 import Home from './pages/home';
 import AddUser from './users/addUser';
+import EditUser from './users/editUser';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route exact path="/adduser" element={<AddUser/>}/>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/adduser" element={<AddUser />} />
+          <Route exact path="/edituser/:id" element={<EditUser />} />
         </Routes>
       </Router>
     </div>
